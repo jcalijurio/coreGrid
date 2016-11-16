cdcec48742a324fdcf468e7ac4ad277cd955ea82
jcalijurio <jorge.calijurio@gmail.com>
2016-11-15 23:28:00 -0200

    Solution Creation

---
+++ test/CoreGrid.Test/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CoreGrid.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void OkTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Theory]
+        [InlineData(4), InlineData(6), InlineData(8)]
+        public void NewTest(int value)
+        {
+            Assert.True(value % 2 == 0);
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+}
